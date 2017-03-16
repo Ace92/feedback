69a48a6c68eab63818c4217b05f664155b608eb6
Ace92 <vilic92@yahoo.se>
2017-03-16 14:54:08 +0100

    Added project folder

---
+++ Feedback_eHealth/mobile/src/main/java/fedback/feedback_ehealth/MainActivity.java
@@ -0,0 +1,13 @@
+package fedback.feedback_ehealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
